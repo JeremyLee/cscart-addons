5e733b22d96d745007d6241e6c7e54135e9d35d4
Jeremy Lee <jeremyduanelee@gmail.com>
2015-08-10 16:52:31 -0400

    Add creating an upgrade package.

--- package.cmd
+++ package.cmd
@@ -1,7 +1,24 @@
 @echo off
+setlocal enabledelayedexpansion
 
-git describe --abbrev=0 --tags > tag
-set /p tag=<tag
-del tag
+git describe --abbrev=0 --tags > temp
+set /p tag=<temp
+
+git describe --abbrev=0 --tags %tag%~^1 > temp
+set /p prevTag=<temp
+
+if defined prevTag (
+  git diff --name-only %prevTag% %tag% > temp
+
+  (set files=)
+  for /f "delims=" %%x in (temp) do call set files=%%files%% %%x
+)
+
+del temp
+
 
 git archive --format zip --output releases/%tag%.zip %tag%
+
+if defined prevTag (
+  git archive --format zip --output releases/%prevTag%-%tag%.zip %tag% %files%
+)
